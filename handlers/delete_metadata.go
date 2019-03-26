a069342d468dcd74f9baa7444067f7b1db7e2114
Will Jaynes <willcj33@gmail.com>
2019-03-26 09:27:56 -0400

    updated some small issues

--- handlers/delete_metadata.go
+++ handlers/delete_metadata.go
@@ -7,7 +7,7 @@ import (
 	"github.com/willcj33/yaml-metadata-exercise/db"
 )
 
-//PutMetadata updates application metadata
+//DeleteMetadata updates application metadata
 func DeleteMetadata(w http.ResponseWriter, r *http.Request, store *db.MetadataStore, config config.Config) {
 	w.Header().Set("Content-Type", "application/text")
 	w.WriteHeader(http.StatusOK)
